cba3a16cd2f70131cdd7e7c2fa63877414536874
ShivamMishra828 <shivam9878522087@gmail.com>
2024-06-25 21:52:39 +0530

    FEAT: Added User Schema with Password Encryption functionality.

---
+++ src/models/user-model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            match: [
+                /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                "Please enter a valid email",
+            ],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+userSchema.pre("save", async function hashPassword(next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hashSync(this.password, 10);
+    next();
+});
+
+userSchema.methods.checkPassword = async function (password) {
+    return await bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
